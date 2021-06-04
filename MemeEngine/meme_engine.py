713423b276c7d0fe683bacfa6ea1318258e7687f
Tomasz Kacmajor <tomasz.kacmajor@gmail.com>
2021-06-04 12:43:19 +0200

    Add random positioning of the text

--- MemeEngine/meme_engine.py
+++ MemeEngine/meme_engine.py
@@ -1,4 +1,6 @@
 """Module providing the engine for memes generation."""
+import random
+
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -13,6 +15,10 @@ class MemeEngine:
         self.output_dir = output_dir
 
     MAX_IMG_WIDTH = 500
+    FONT_SIZE = random.randint(20, 30)
+    DIST_BETWEEN_TEXT_AND_AUTHOR = 40
+
+
 
     def make_meme(self, img_path: str, text: str, author: str, width=500) -> str:
         """Generate meme based on provided image and text.
@@ -31,16 +37,38 @@ class MemeEngine:
 
         with Image.open(img_path) as img:
             img = proportional_resize(img, width)
-            fnt = ImageFont.truetype("./fonts/LilitaOne-Regular.ttf", 30)
+            fnt = ImageFont.truetype("./fonts/LilitaOne-Regular.ttf", self.FONT_SIZE)
             d = ImageDraw.Draw(img)
-            d.text((10, 60), text, font=fnt, fill=(255, 255, 255))
-            d.text((10, 100), author, font=fnt, fill=(255, 255, 255))
+
+            pos_x, pos_y = self.get_random_text_pos(img, max(len(text), len(author)))
+            d.text((pos_x, pos_y), text, font=fnt, fill=(255, 255, 255))
+            d.text((pos_x, pos_y + self.DIST_BETWEEN_TEXT_AND_AUTHOR), author, font=fnt, fill=(255, 255, 255))
             img.save(output_path)
 
         return output_path
 
+    def get_random_text_pos(self, img: Image, max_text_len: int) -> (int, int):
+        """Get random text position for the image, considering its size and margins.
 
-def proportional_resize(img: Image, width: int):
+        @param img: Image on which the text will be drawn.
+        @param max_text_len: Maximum length of lines in the text to be drawn.
+        @return: Tuple of (x, y) position of the text.
+        """
+        MARGINS_SIZE = 10
+
+        # Experimantally chosen average width of one letter.
+        width_of_a_letter = self.FONT_SIZE / 2
+
+        width = img.size[0]
+        height = img.size[1]
+        text_size = max_text_len * width_of_a_letter
+        x_random_range = (MARGINS_SIZE, width - MARGINS_SIZE - text_size)
+        y_random_range = (MARGINS_SIZE, height - MARGINS_SIZE - self.DIST_BETWEEN_TEXT_AND_AUTHOR)
+
+        return random.randint(*x_random_range), random.randint(*y_random_range)
+
+
+def proportional_resize(img: Image, width: int) -> Image:
     """Resize the image proportionally, based on desired width.
 
     @param img: Image to be resized.
@@ -51,4 +79,3 @@ def proportional_resize(img: Image, width: int):
     height = int((float(img.size[1]) * float(percent)))
     img = img.resize((width, height), Image.ANTIALIAS)
     return img
-
